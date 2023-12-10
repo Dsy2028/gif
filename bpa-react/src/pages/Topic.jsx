cab75a5fbde37b61f85ea47d5df77d80aef7564b
Dsy2028 <126633481+Dsy2028@users.noreply.github.com>
2023-12-10 01:34:28 -0500

    f

--- bpa-react/src/pages/Topic.jsx
+++ bpa-react/src/pages/Topic.jsx
@@ -3,6 +3,7 @@ import { useParams, useLocation, Link } from "react-router-dom";
 import science from "../imgs/genetic-data-svgrepo-com.svg";
 import endless from "../imgs/endless-constellation.svg";
 import flat from "../imgs/flat-mountains.svg";
+import snow from "../imgs/snow.jpg"
 
 export default function Topic() {
   const { topicName } = useParams();
@@ -12,13 +13,20 @@ export default function Topic() {
   const location = useLocation();
 
   const pageNames = {
-    "/courses/Algebra%201/Real%20Number": "Algebra 1",
-    "/classes": "Classes",
-    "/calender": "Calender",
+    "/courses/Algebra%201": "Algebra 1",
+    "/courses/Geometry": "Geometry",
+    "/courses/Statistics": "Statistics",
+    "/courses/Calculus": "Calculus",
+    "/courses/Trigonometry": "Trigonometry",
+  };
+  const getPageName = () => {
+    const pathSegments = location.pathname.split('/');
+    const currentPath = `/${pathSegments[1]}/${pathSegments[2]}`;
+    return pageNames[currentPath] || '';
   };
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/topics/${topicId}/question/${questionId}`)
+    fetch(`http://localhost:3000/api/topics/656604348bc2ccb633300115/question/656603178bc2ccb633300114`)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -34,7 +42,7 @@ export default function Topic() {
       });
   }, [topicName]);
 
-  const getPageName = () => pageNames[location.pathname] || "";
+
 
   const lessons = [
     {
@@ -44,17 +52,18 @@ export default function Topic() {
       position: "relative",
     },
     {
-      title: "Intro to Real Numbers",
+      title: "Recap",
       link: "/link-to-lesson-2",
       icon: "fa-pause",
       position: "relative",
       marginLeft: "60px",
-      marginTop: "40px"
+      marginTop: "40px",
+      
     },
     {
-        title: "Intro to Real Numbers",
+        title: "Practice Questions",
         link: "/link-to-lesson-2",
-        icon: "fa-pause",
+        icon: "fa-pencil",
         position: "relative",
         marginLeft: "140px",
         marginTop: "40px"
@@ -62,7 +71,7 @@ export default function Topic() {
       {
         title: "Intro to Real Numbers",
         link: "/link-to-lesson-2",
-        icon: "fa-pause",
+        icon: "fa-pencil",
         position: "relative",
         marginLeft: "250px",
         marginTop: "40px"
@@ -70,7 +79,7 @@ export default function Topic() {
       {
         title: "Intro to Real Numbers",
         link: "/link-to-lesson-2",
-        icon: "fa-pause",
+        icon: "fa-file",
         position: "relative",
         marginLeft: "140px",
         marginTop: "40px"
@@ -87,7 +96,7 @@ export default function Topic() {
 
   return (
     <>
-      <div className=" h-screen pl-56 pr-56 flex">
+      <div className="  pl-56 pr-56 flex">
         <div className="h-full fixed">
           <div className="flex items-center mb-7">
             <h1 className="text-semibold text-lg nunito mr-3">Courses</h1>
@@ -95,6 +104,10 @@ export default function Topic() {
             <h1 className="text-semibold text-lg nunito ml-3">
               {getPageName()}
             </h1>
+            <i class="fa-solid fa-chevron-right fa-sm ml-3"></i>
+            { question &&
+                <h1 className="text-semibold text-lg nunito ml-3">{question.topicName}</h1>
+                }
           </div>
           <div className="w-96 outline p-5 h-80 ">
             <div className="flex h-14">
@@ -104,21 +117,23 @@ export default function Topic() {
               <h1>{getPageName()}</h1>
             </div>
             <div className="text-3xl">
-              <h1>REal numbers</h1>
+                { question &&
+                <h1>{question.topicName}</h1>
+                }
             </div>
             <p>Understand the use of REal Numbers</p>
             <p>10 lessons</p>
           </div>
         </div>
         <div
-          className="min-h-[1000px] w-[800px] ml mb-2 p-8 flex justify-center"
+          className="min-h-full w-[800px] ml mb-2 p-8 flex justify-center"
           style={{
             backgroundImage: ` url(${flat})`,
             backgroundPosition: " bottom",
             backgroundRepeat: "no-repeat",
           }}
         >
-          <div className=" w-[480px] z-50 flex flex-col">
+          <div className=" w-[480px] z-50 flex flex-col mb-9">
             {lessons.map((lesson, index) => (
               <div
                 key={index}
@@ -132,16 +147,20 @@ export default function Topic() {
                     ></i>
                   </div>
                 </div>
-                <h1 className="mt-2">{lesson.title}</h1>
+                <h1 className="mt-2 text-center">{lesson.title}</h1>
                 <div className="flex items-center justify-center mt-1">
                   <Link to={lesson.link}>
-                    <button className="main-color rounded w-20 text-white">
+                    <button className="main-color rounded w-20 text-white hover:bg-blue-700">
                       Go
                     </button>
                   </Link>
                 </div>
               </div>
             ))}
+            <div>
+                <h1>Next Course</h1>
+            </div>
+
           </div>
         </div>
       </div>
