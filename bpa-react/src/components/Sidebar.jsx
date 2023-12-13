96c42fd14b93a9dbffde2b78c1625be1f0336161
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2023-12-13 12:13:55 -0500

    progress with chat

---
+++ bpa-react/src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+
+const Sidebar = () =>{
+    return(
+        <div>Sidebar</div>
+    )
+}
+export default Sidebar
--- bpa-react/src/pages/Chat.jsx
+++ bpa-react/src/pages/Chat.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import DashNav from '../components/DashNav'
 import DashboardNav from '../components/DashboardNav'
 export default function Chat() {
+    return (
+        <h1>Hello World</h1>
+    )
 
 }
