a864d6e90584a59383c02b8a9a649c1ba868300b
susmitaadhikari2006 <115734117+susmitaadhikari2006@users.noreply.github.com>
2023-12-15 12:59:05 -0500

    asdrftgyhju

--- bpa-react/src/components/Chats.jsx
+++ bpa-react/src/components/Chats.jsx
@@ -10,6 +10,20 @@ const Chats = () => {
           <p>Hello world</p>
         </div>
       </div>
+      <div className="userChat">
+        <img src="https://retratosbarcelona.com/wp-content/uploads/2022/09/Retratos-Barcelona-Linkedin-Photography-Alejandra.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Person</span>
+          <p>Hello world</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://retratosbarcelona.com/wp-content/uploads/2022/09/Retratos-Barcelona-Linkedin-Photography-Alejandra.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Person</span>
+          <p>Hello world</p>
+        </div>
+      </div>
     </div>
   )
 }
