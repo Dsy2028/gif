f0752bb511aec79eefa731601f978115eb9b9d6b
susmitaadhikari2006 <115734117+susmitaadhikari2006@users.noreply.github.com>
2023-12-15 14:09:29 -0500

    Update Input.jsx

--- bpa-react/src/components/Input.jsx
+++ bpa-react/src/components/Input.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Input = () => {
   return (
-    <div className='input'>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Type Something...' />
+      <div className="send">
+        <img src="" alt="" />
+        <input type="file" style={{display:"none"}} id='file' />
+        <label htmlFor="file">
+          <img src="" alt="" />
+        </label>
+        <button>send</button>
+      </div>
+    </div>
   )
 }
 
