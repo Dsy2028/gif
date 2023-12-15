3ae86d587144ec432eb67c1a3793f60570f10071
susmitaadhikari2006 <115734117+susmitaadhikari2006@users.noreply.github.com>
2023-12-15 12:22:33 -0500

    Under Construction

--- bpa-react/src/components/Sidebar.jsx
+++ bpa-react/src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import react from "react";
 import ChatNavBar from "./ChatNavBar";
+import Search from "./Search";
 
 const Sidebar = () =>{
     return(
         <div className='sidebar'>
             <ChatNavBar/>
+            <Search/>
         </div>
     )
 }
