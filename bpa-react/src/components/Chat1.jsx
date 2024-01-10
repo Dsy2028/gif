cb8da5ecd89b5e95c2f145fd8d6b8b313db1c7f2
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-01-10 13:28:28 -0500

    importing neceserry things to chat

--- bpa-react/src/components/Chat1.jsx
+++ bpa-react/src/components/Chat1.jsx
@@ -4,6 +4,12 @@ import More from "../imgs/more.png";
 import Messages from './Messages';
 import Input from './Input';
 //import { Input } from 'postcss';
+import GoogleAuth from "../components/GoogleAuth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { logOutUserSuccess, logOutUserStart, logOutUserFailure } from '../redux/user/userSlice';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { app } from '../firebase';
+import { updateUserSuccess, deleteUserSuccess, deleteUserFailure, deleteUserStart, updateUserStart } from '../redux/user/userSlice.js';
 
 const Chat1 = () => {
   return (
