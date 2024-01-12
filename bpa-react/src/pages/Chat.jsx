f68ad98494915d04eefbb3e39f1fab4411479b3a
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-01-12 14:09:11 -0500

    Update Chat.jsx

--- bpa-react/src/pages/Chat.jsx
+++ bpa-react/src/pages/Chat.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar';
 import Chat1 from '../components/Chat1';
+import Footer from "../components/Footer";
 import style from '../style.scss';
 
 const Chat = () => {
-    return(
-        <div className='home'>
-            <div className ="container">
-                <Sidebar/>
-                <Chat1/>
+    return (
+        <div>
+            <div className='home'>
+                <div className="container">
+                    <Sidebar />
+                    <Chat1 />
+                </div>
             </div>
+            <Footer />
         </div>
+
     )
 }
 export default Chat
